500c1f1f283a2036cba471745c9611a70dea323f
Heungsoo Lee <heungsoo1@gmail.com>
2022-01-13 11:26:49 +0900

    change state with reducer

--- src/context/provider.tsx
+++ src/context/provider.tsx
@@ -1,30 +1,67 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useReducer } from 'react';
 
 type PropsType = {
   children: JSX.Element;
 };
 
-const defaultCount = {
+type DefaultCountType = {
+  count: number;
+  increaseCount: () => void;
+  decreaseCount: () => void;
+};
+
+type InitialStateType = {
+  count: number;
+};
+
+type ActionType = {
+  type: 'INCREASE_COUNT' | 'DECREASE_COUNT';
+  payload: number;
+};
+
+const initialState: InitialStateType = {
   count: 0,
-  increaseCount: (): void => {},
-  decreaseCount: (): void => {},
+};
+
+const defaultCount: DefaultCountType = {
+  count: 0,
+  increaseCount: () => {},
+  decreaseCount: () => {},
 };
 
 const CountContext = createContext(defaultCount);
 
-function CountProvider({ children }: PropsType): JSX.Element {
-  const [count, setCount] = useState<number>(0);
+const reducer = (state: InitialStateType, action: ActionType) => {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {
+        count: action.payload,
+      };
+    case 'DECREASE_COUNT':
+      return {
+        count: action.payload,
+      };
 
+    default:
+      return state;
+  }
+};
+
+function CountProvider({ children }: PropsType): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
   const increaseCount = (): void => {
-    setCount(count + 1);
+    dispatch({ type: 'INCREASE_COUNT', payload: state.count + 1 });
   };
 
   const decreaseCount = (): void => {
-    setCount(count - 1);
+    dispatch({ type: 'DECREASE_COUNT', payload: state.count - 1 });
   };
 
   return (
-    <CountContext.Provider value={{ count, increaseCount, decreaseCount }}>
+    <CountContext.Provider
+      value={{ count: state.count, increaseCount, decreaseCount }}
+    >
       {children}
     </CountContext.Provider>
   );
